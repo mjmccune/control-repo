be287b78e43ea18ff3a4956fadbb2a50896a4876
mjmccune <michael.mccune@nmi.com>
2021-04-30 16:09:59 +0000

    Added rspec

--- site/rspec_example/manifests/init.pp
+++ site/rspec_example/manifests/init.pp
@@ -1,8 +1,6 @@
-# rspec_example
+# @summary A short summary of the purpose of this class
 #
 # A description of what this class does
-#
-# @summary A short summary of the purpose of this class
 #
 # @example
 #   include rspec_example
